6c15b6523dba2c5c78e88b7765d107eb82ee2027
James McCaughey <jmccaughey@kirbyvillecisd.org>
2018-06-07 15:49:55 -0500

    Removed duplicate files, added some comments.

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
-require_once('inc/header.php');
+/* ---------------------------------------------------------------------------------------------------------- */
+/* Filename: index.php                                                                                        */
+/* Author: James McCaughey                                                                                    */
+/* E-Mail: jmccaughey@kirbyvillecisd.org                                                                      */
+/*                                                                                                            */
+/* This file is the root of the application and loads the main application.                                   */
+/* ---------------------------------------------------------------------------------------------------------- */
 
-echo "\t\t<section>&nbsp;</section>\n";
+require_once('inc/header.php'); // Require the header which loads the top of the page and the meta data.
 
+echo "\t\t<section>&nbsp;</section>\n"; // Add a section tag where dynamic content will be loaded.
 
+// List our active users who are currently using the application.
 if ( (SHOW_LOGGED_ON_USERS == true && !isMobile()) || (MOBILE_SHOW_LOGGED_ON_USERS == true && isMobile()) ) {
 	$result = $mysqli->query("SELECT `name` FROM `active_users` WHERE `date` > date_sub(now(), interval 5 minute);");
 	while ($row=$result->fetch_assoc()) {
@@ -18,6 +26,6 @@ if ( (SHOW_LOGGED_ON_USERS == true && !isMobile()) || (MOBILE_SHOW_LOGGED_ON_USE
 	echo "\t\t<footer><h3>Active Users:</h3>".( isset($active_users) ? $active_users : "<i>No Active Users</i>")."</footer>\n";
 }
 
-require_once('inc/footer.php');
+require_once('inc/footer.php'); // Require the footer which closes out our HTML and anything else left open.
 
 ?>
